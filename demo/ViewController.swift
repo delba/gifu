0bd0bf1e3739bdb912f2995cb69c9d6c144f6cbe
Reda Lemeden <reda@thoughtbot.com>
2014-09-10 21:28:19 +0200

    Merge demo

---
+++ demo/ViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var button: FlatButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    imageView.setAnimatableImage(named: "mugen.gif")
+    UIApplication.sharedApplication().setStatusBarStyle(.LightContent, animated: false)
+  }
+
+  @IBAction func toggleAnimation(button: UIButton) {
+    if imageView.isAnimating {
+      imageView.stopAnimating()
+      button.layer.backgroundColor = UIColor.whiteColor().CGColor
+      button.setTitleColor(UIColor.blackColor(), forState: .Normal)
+    } else {
+      imageView.startAnimating()
+      button.layer.backgroundColor = UIColor.clearColor().CGColor
+      button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+    }
+  }
+}
+
